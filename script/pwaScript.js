78ae4d7447610e74a805b73fceab5cd00de27fbd
Vladyslav <ganushkevychv@gmail.com>
2019-11-07 22:31:10 +0200

    install button added and script corrected to button

--- script/pwaScript.js
+++ script/pwaScript.js
@@ -1,16 +1,25 @@
 
     let deferredPrompt = null;
+    let setupButton;
 
     window.addEventListener('beforeinstallprompt', (e) => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
+
+      if (setupButton == undefined) {
+        setupButton = document.getElementById("install-btn");
+        }
+        // Show the setup button
+        setupButton.style.display = "inline";
+        setupButton.disabled = false;
     });
     
     async function install() {
       if (deferredPrompt) {
         deferredPrompt.prompt();
+        setupButton.disabled = true;
         console.log(deferredPrompt)
         deferredPrompt.userChoice.then(function(choiceResult){
     
